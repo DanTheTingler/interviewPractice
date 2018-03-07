ed2bc0609de679edaa502ff6fdacf284d5b33566
Daniel <dting@ucsd.edu>
2018-03-06 16:05:36 -0800

    added some simple notes on Python identity and membership operators

---
+++ Python/operators.py
@@ -0,0 +1,14 @@
+#Identity operators
+#'is' -> returns true if the operands are identical
+#'is not' -> returns true if operands are NOT identical (do not refer to the same object)
+#Example:
+x = True 
+x is True #this will return true
+
+#Membership operators
+#'in' and 'not in'
+#Example:
+phrase = "Hello World"
+
+if('H' in phrase):
+   print('H was found!')
