3ffab57340bdc56dcbe8d8a10a11d2b240b67766
Daniel <dting@ucsd.edu>
2018-03-08 23:34:31 -0800

    added an example of string interpolation in Javascript ES6

---
+++ JavaScript/stringInterpolationExample.js
@@ -0,0 +1,8 @@
+//uses backticks (`) and ${} to interpolate strings
+//used in JavaScript ES6
+
+let myName = 'Daniel';
+let myCity = 'San Diego';
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
+
