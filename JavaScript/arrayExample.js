dd48371101e6fc743d1e7bf0b6a1fc0799026278
Daniel <dting@ucsd.edu>
2018-03-08 23:40:11 -0800

    added examples of arrays

---
+++ JavaScript/arrayExample.js
@@ -0,0 +1,15 @@
+let newYearsResolutions = ['Get a job', 'Get closer to Jesus', 'Get a life'];
+
+var listItem = newYearsResolutions[0];
+console.log(listItem);
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
+
+newYearsResolutions[1] = 'Learn a new language';
+
+console.log(newYearsResolutions.length)
+
+newYearsResolutions.push('Get yoked', 'Learn how to box');
+console.log(newYearsResolutions);
+newYearsResolutions.pop();
+console.log(newYearsResolutions);
