ce9774168e10a3f1df466cb563c8025da8a3c2f5
Daniel <dting@ucsd.edu>
2018-03-13 16:09:41 -0700

    added example of using the .map functions to create new arrays

---
+++ JavaScript/arrayMappingExample.js
@@ -0,0 +1,15 @@
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+let secretMessage = animals.map(function(words){
+  return words[0];
+});
+
+let secretMessage = animals.map(words => words[0]);
+
+console.log(secretMessage.join(''));
+
+let bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+let smallNumbers = bigNumbers.map(num => num/100);
